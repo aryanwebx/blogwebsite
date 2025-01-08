bcd743cb8c80bb19dadfb316161630bb13066859
Aryan Yadav <aryanyadav9811@gmail.com>
2025-01-08 22:30:31 +0530

    modify the previous and next button functionality and add new functionality which remind user to make notes

--- index.js
+++ index.js
@@ -47,52 +47,68 @@ let blogs = [
 
 let blogContainer = document.getElementById('blogContainer');
 
+let Title = document.createElement('h1');
+let Author = document.createElement('h2');
+let Category = document.createElement('h2');
+let date = document.createElement('h3');
+let content = document.createElement('p');
 
-    let Title = document.createElement('h1');
-    let Author = document.createElement('h2');
-    let Category = document.createElement('h2');
-    let date = document.createElement('h3');
-    let content = document.createElement('p');
+Title.innerHTML = blogs[0].title;
+Author.innerHTML = `Author: ${blogs[0].author}`;
+Category.innerHTML = `Category: ${blogs[0].category}`;
+date.innerHTML = `Date: ${blogs[0].date}`;
+content.innerHTML = blogs[0].content;
 
-    Title.innerHTML = blogs[0].title;
-    Author.innerHTML = `Author: ${blogs[0].author}`;
-    Category.innerHTML = `Category: ${blogs[0].category}`;
-    date.innerHTML = `Date: ${blogs[0].date}`;
-    content.innerHTML = blogs[0].content;
-
-    blogContainer.append(Title, Author, Category, date, content);
+blogContainer.prepend(Title, Author, Category, date, content);
 
 
 // Previous and next button functionality
 
-let navigationButton=document.getElementById("navigation-button");
-let index=1;
+let navigationButton = document.getElementById("navigation-button");
+let index = 1;
 
-navigationButton.addEventListener('click',(event)=>{
-    
-    if(event.target.id==='next' && index<6)
-    {
-       
+navigationButton.addEventListener('click', (event) => {
+
+
+    if (event.target.id === 'next' && index < 6) {
+
         Title.innerHTML = blogs[index].title;
         Author.innerHTML = `Author: ${blogs[index].author}`;
         Category.innerHTML = `Category: ${blogs[index].category}`;
         date.innerHTML = `Date: ${blogs[index].date}`;
         content.innerHTML = blogs[index].content;
-        blogContainer.append(Title, Author, Category, date, content);
-        index=index+1;
+        blogContainer.prepend(Title, Author, Category, date, content);
+        index = index + 1;
 
     }
-    else if(event.target.id==='previous' && index>0)
-    {
-       
-        index=index-1;
+    else if (event.target.id === 'previous' && index > 0) {
+
+        index = index - 1;
         Title.innerHTML = blogs[index].title;
         Author.innerHTML = `Author: ${blogs[index].author}`;
         Category.innerHTML = `Category: ${blogs[index].category}`;
         date.innerHTML = `Date: ${blogs[index].date}`;
         content.innerHTML = blogs[index].content;
-        blogContainer.append(Title, Author, Category, date, content);
-        
+        blogContainer.prepend(Title, Author, Category, date, content);
 
     }
-})
+
+    document.documentElement.scrollTop = 0; // For modern browsers
+    document.body.scrollTop = 0; // For older browsers
+});
+
+//function which always remind user to take note from blog
+let noteReminder = document.getElementsByClassName("note-remind");
+console.log(noteReminder)
+let display = 1;
+setInterval(() => {
+
+    if (display % 2 == 0) {
+        noteReminder[0].style.display = "block";
+        display = display + 1;
+    }
+    else {
+        noteReminder[0].style.display = "none";
+        display = display + 1;
+    }
+}, 2000) 
